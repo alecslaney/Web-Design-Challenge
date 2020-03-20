6c99f277a9a9b8ad519f7d539566657ef19e3c62
Alec Slaney <alecg.sla@gmail.com>
2020-03-20 15:14:03 -0700

    Removed extra index from data page

--- WebVisualizations/resources/data_export.py
+++ WebVisualizations/resources/data_export.py
@@ -2,9 +2,9 @@
 import pandas as pd
 
 #path to data csv, conversion from pandas frame to html
-data = "WebVisualizations/resources/cities.csv"
+data = "cities.csv"
 data = pd.read_csv(data)
-html = data.to_html()
+html = data.to_html(index=False)
 
 #write html to text file
 text_file = open("data_html.txt", "w")
